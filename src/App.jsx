d30a116bfeac2cd655ae7e6302ffda612e83eee7
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-03-12 17:45:54 +0100

    Add Personal Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './styles/App.css'
+import Card from './components/Card'
+import Personal from './components/Personal'
 
 function App() {
+  const [personal, setPersonal] = useState({ 
+    name: "John Doe",
+    email: "johnny@johnny.com",
+    phoneNumber: 6666666,
+    address: "Amsterdam"
+  })
 
   return (
     <>
       <header>
         <h1>CV Creator ✏️</h1>
       </header>
+      <main>
+        <div id='sidebar'>
+          <Card>
+            <Personal 
+              state={personal}
+              setState={setPersonal}
+            />
+          </Card>
+        </div>
+        <div id='previewSection'>
+          <h1>{personal.name}</h1>
+          <h3>{personal.email}</h3>
+        </div>
+      </main>
      
     </>
   )
